8a08e0ee36a4e758bfd3ee30e04aa194e326e6c3
wilmerSD <josewilmersanchezdiaz@MacBook-Pro-de-JOSE.local>
2025-02-01 17:22:12 -0500

    Prueba unitaria con JUnit

--- src/test/java/com/reto/AppTest.java
+++ src/test/java/com/reto/AppTest.java
@@ -1,5 +1,28 @@
 package com.reto;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.reto.model.Animal;
+import com.reto.model.AnimalTerrestre;
+import com.reto.model.AnimalVolador;
+import com.reto.service.AnimalService;
+
 public class AppTest {
-    
+    @Test
+    public void testAgruparAnimalesPorTipo() {
+        AnimalService service = new AnimalService();
+        List<Animal> animales = List.of(
+                new AnimalTerrestre("Perro", "Guau"),
+                new AnimalVolador("Águila", "Crii"),
+                new AnimalTerrestre("Gato", "Miau"));
+
+        Map<String, List<Animal>> resultado = service.agruparAnimalesPorTipo(animales);
+        assertEquals(2, resultado.get("Terrestre").size());
+        assertEquals(1, resultado.get("Volador").size());
+    }
 }
